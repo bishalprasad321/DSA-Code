7800ed4c30a369389c38ce168379e3f3481a3377
bishalprasad321 <bishalprasad321@gmail.com>
2022-06-20 23:03:43 +0530

    modified

--- Heap/01_heap_implementation.cpp
+++ Heap/01_heap_implementation.cpp
@@ -45,35 +45,78 @@ public:
     }
 
     // modification functions
-    void insert(int value)
-    {
-        size = size + 1;
-        int index = size;
+    void insert(int value);
 
-        arr[index] = value;
-
-        while (index > 1)
-        {
-            int parent = index / 2;
-
-            if (arr[parent] < arr[index])
-            {
-                swap(arr[parent], arr[index]);
-                index = parent;
-            }
-            else
-                return;
-        }
-    }
+    void deleteFromHeap();
 
     // output function
-    void printHeap()
-    {
-        for (int i = 1; i <= size; i++)
-            cout << arr[i] << " ";
-        cout << endl;
-    }
+    void printHeap();
 };
+
+void Heap ::insert(int value)
+{
+    size = size + 1;
+    int index = size;
+
+    arr[index] = value;
+
+    while (index > 1)
+    {
+        int parent = index / 2;
+
+        if (arr[parent] < arr[index])
+        {
+            swap(arr[parent], arr[index]);
+            index = parent;
+        }
+        else
+            return;
+    }
+}
+
+void Heap :: deleteFromHeap()
+{
+    if (size == 0)
+    {
+        cout << "There is nothing to delete" << endl;
+        return ;
+    }
+
+    arr[1] = arr[size];
+    size--;
+
+    int i = 1;
+    while (i < size)
+    {
+        int leftIndex = 2*i;
+        int rightIndex = 2*i + 1;
+
+        if (leftIndex < size and arr[i] < arr[leftIndex])
+        {
+            swap(arr[leftIndex], arr[i]);
+            
+            i = leftIndex;
+        }
+
+        else if(rightIndex < size and arr[i] < arr[rightIndex])
+        {
+            swap(arr[rightIndex], arr[i]);
+
+            i = rightIndex;
+        }
+
+        else
+            return ;
+
+    }
+}
+
+void Heap :: printHeap()
+{
+    for (int i = 1; i <= size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
 
 int main()
 {
@@ -83,6 +126,14 @@ int main()
     h.insert(50);
     h.insert(40);
     h.insert(20);
+
+    cout << "After insertion of elements inside the heap" <<endl;
+
+    h.printHeap();
+
+    h.deleteFromHeap();
+
+    cout << "After deletion of element from the heap" << endl;
 
     h.printHeap();
 
